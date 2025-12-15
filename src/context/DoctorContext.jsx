ccabc8b8319f5e319ba2c24f4cb448b9d3e94a12
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-15 23:09:16 +0500

    doctor dashboard data is fetched

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -8,6 +8,7 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [dToken,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : "");
     const [appointments, setAppointments] = useState([]);
+    const [dashData, setDashData] = useState(false);
 
     const getAllAppointments = async () => {
        try {
@@ -54,13 +55,30 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getDashData = async() => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/doctor/dashboard",{headers:{dToken}})
+            if(data.success) {
+                setDashData(data.dashData);
+                console.log("dashboard data is", data.dashData)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken, setDToken,
         backendUrl, 
         appointments, setAppointments,
         getAllAppointments,
         completeAppointment, 
-        cancelAppointment
+        cancelAppointment,
+        dashData, setDashData,
+        getDashData,
     }
 
     return(
