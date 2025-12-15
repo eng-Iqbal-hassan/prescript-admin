a72ca3aeefe0cce96cd64a5e8a92c313b39d3519
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-15 16:32:13 +0500

    appointment cancel and appointment complete apis are called

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -13,8 +13,8 @@ const DoctorContextProvider = (props) => {
        try {
         const {data} = await axios.get(backendUrl + "/api/doctor/appointments", {headers:{dToken}});
         if(data.success) {
-            setAppointments(data.appointments.reverse());
-            console.log(data.appointments.reverse())
+            setAppointments(data.appointments);
+            console.log(data.appointments)
         } else {
             toast.error(data.message);
         }
@@ -24,11 +24,43 @@ const DoctorContextProvider = (props) => {
        } 
     }
 
+    const completeAppointment = async(appointmentId) => {
+        try {
+            const {data} = await axios.post(backendUrl+"/api/doctor/appointment-complete",{appointmentId}, {headers:{dToken}})
+            if(data.success) {
+                toast.success(data.message);
+                getAllAppointments();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
+    const cancelAppointment = async(appointmentId) => {
+        try {
+            const {data} = await axios.post(backendUrl+"/api/doctor/appointment-cancel",{appointmentId}, {headers:{dToken}})
+            if(data.success) {
+                toast.success(data.message);
+                getAllAppointments();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken, setDToken,
         backendUrl, 
         appointments, setAppointments,
-        getAllAppointments
+        getAllAppointments,
+        completeAppointment, 
+        cancelAppointment
     }
 
     return(
