03c340d62d34cbe4d8ceab2e2dd635e777dff280
eng-Iqbal-hassan <eng.iqbal110@gmail.com>
2025-12-15 15:57:17 +0500

    data for getting all appointments of a specific doctor is fetched

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -1,15 +1,34 @@
 import { createContext, useState } from "react";
-
 export const DoctorContext = createContext();
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const DoctorContextProvider = (props) => {
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [dToken,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : "");
+    const [appointments, setAppointments] = useState([]);
+
+    const getAllAppointments = async () => {
+       try {
+        const {data} = await axios.get(backendUrl + "/api/doctor/appointments", {headers:{dToken}});
+        if(data.success) {
+            setAppointments(data.appointments.reverse());
+            console.log(data.appointments.reverse())
+        } else {
+            toast.error(data.message);
+        }
+       } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+       } 
+    }
 
     const value = {
         dToken, setDToken,
-        backendUrl
+        backendUrl, 
+        appointments, setAppointments,
+        getAllAppointments
     }
 
     return(
